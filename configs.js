bf2933e4156acb38178b9e477cd9d947de429a5d
maliemin-Mstar <maliemin.jj@gmail.com>
2015-01-12 11:27:41 +0800

    first commit

---
+++ configs.js
@@ -0,0 +1,22 @@
+var airpubConfigs = {
+  // blog name
+  name: 'blog',
+  // just a little description
+  description: '马列敏的blog',
+  // blog uri
+  url: 'http://maliemin-Mstar.github.io',
+  // blog theme
+  // theme: 'my-theme',
+  // for images upload
+  upyun: {
+    bucket: 'mybucket',
+    uri: 'http://{{bucketName}}.b0.upaiyun.com',
+    endpoint: 'http://v0.api.upyun.com/{{bucketName}}',
+    form_api_secret: 'xxxxxx'
+  }
+}
+
+// for backend service
+var duoshuoQuery = {
+  short_name: 'maliemin-mstar'
+};
